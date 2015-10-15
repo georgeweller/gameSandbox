6a801b1ef9d55fa42d8a51e577622fab66df62c6
georgeweller <george.weller.1@gmail.com>
2015-10-15 15:03:18 +0100

    In html5Game.js, abstract the redrawing into a draw() function.

--- html5Game.js
+++ html5Game.js
@@ -6,19 +6,25 @@ var avatar = new Image();
 avatar.src = "Images/avatar.png";
 avatar.onload = function(){setup();};
 
+var canvasWidth = 600;
+
 function setup(){
-	canvas.width = 600;
+	canvas.width = canvasWidth;
 	canvas.height = 400;
 	ctx.drawImage(avatar,10,10);
 }
 
-requestAnimationFrame(updateCanvas);//Whenever requestAnimationFrame is called on a function, requestAnimationFrame passes an argument to 
+requestAnimationFrame(gameLoop);//Whenever requestAnimationFrame is called on a function, requestAnimationFrame passes an argument to 
 //that function which is the timestamp of them the function is called.
 
-function updateCanvas(timeStamp){
-	canvas.width+=1;
-	if(canvas.width<1000){
-		requestAnimationFrame(updateCanvas);//requestAnimationFrame calls stretchCanvas and passes the current time as an argument
+function gameLoop(timeStamp){
+	canvasWidth+=1;
+	draw();
+	if(canvasWidth<1000){
+		requestAnimationFrame(gameLoop);//requestAnimationFrame calls stretchCanvas and passes the current time as an argument
 	}
 }
 
+function draw(){
+	canvas.width=canvasWidth;
+}
